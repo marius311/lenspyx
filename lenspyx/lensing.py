9bd5bad4a5bba9e016f93e4b0817832c40741ed5
shuimufge <fge@ucdavis.edu>
2023-02-07 14:22:41 -0800

    enable T lensing on strips

--- lenspyx/lensing.py
+++ lenspyx/lensing.py
@@ -80,7 +80,46 @@ def alm2lenmap_spin(gclm, dlms, nside, spin, nband=8, facres=-1, verbose=True):
     return ret.real, ret.imag
 
 ### Modified by FG
-def strip_alm2lenmap_spin(gclm, dlms, nside, spin, theta1, theta2, nband=4, facres=-1, verbose=True):
+
+def strip_alm2lenmap(alm, dlms, nside, theta1, theta2, nband=8, facres=0, verbose=True, symmetric_band=True):
+    r"""Computes a deflected spin-0 healpix map from its alm and deflection field alm.
+
+        Args:
+            alm: undeflected map healpy alm array
+            dlms: The spin-1 deflection, in the form of either a list of two healpy alm arrays or a list of two healpix maps.
+
+                    In the former case the two arrays are the gradient and curl deflection healpy alms.
+                    (e.g. :math:`\sqrt{L(L+1)}\phi_{LM}` with :math:`\phi` the lensing potential)
+                    The curl can be set to None if irrelevant.
+
+                    In the latter case the two arrays are the real and imag. part of the precomputed spin-1 deflection.
+                    (e.g. :math:`-\sum_{LM}\sqrt{L(L+1)}\phi_{LM} \:_{1}Y_{LM}(\hat n))`
+                    This saves some computation time as this operation must be performed anyways.
+                    In this case, their healpix resolution must match the argument *nside*.
+
+            nside: desired healpix resolution of the deflected map
+            theta1, theta2: The range of the strip 
+                            These theta's are defined on the southern hemisphere for spt3g project
+                    If theta1 < theta2, the strip is [theta1, theta2]
+                    if theta1 > theta2, the strip is [theta2, theta1]
+            facres(optional): the deflected map is constructed by interpolation of the undeflected map,
+                              built at target res. ~ :math:`0.7 * 2^{\rm facres}.` arcmin
+            nband(optional): To avoid dealing with too many large maps in memory, the operations is split in bands.
+            verbose(optional): If set, prints a bunch of timing and other info. Defaults to true.
+            symmetric_band(optional): If set True, do lensing on the symmetrical bands on north and south hemisphere
+                                      If set False, only do strip lensing on one hemisphere
+
+        Returns:
+            Deflected healpy map at resolution nside.
+
+
+    """
+    assert len(dlms) == 2
+    if not np.iscomplexobj(dlms[0]): assert dlms[0].size == dlms[1].size and dlms[0].size == hp.nside2npix(nside)
+    return _strip_lens_gclm_sym_timed(0, dlms[0], -alm, nside, theta1, theta2, dclm=dlms[1], nband=nband, facres=facres, verbose=verbose, symmetric_band=symmetric_band)
+
+
+def strip_alm2lenmap_spin(gclm, dlms, nside, spin, theta1, theta2, nband=4, facres=-1, verbose=True, symmetric_band=True):
     r"""Computes a deflected spin-weight Healpix map from its gradient and curl modes and deflection field alm.
 
         Args:
@@ -100,11 +139,17 @@ def strip_alm2lenmap_spin(gclm, dlms, nside, spin, theta1, theta2, nband=4, facr
 
             nside: desired healpix resolution of the deflected map
             spin: spin-weight of the maps to deflect (2 for polarization).
+            theta1, theta2: The range of the strip 
+                            These theta's are defined on the southern hemisphere for spt3g project
+                    If theta1 < theta2, the strip is [theta1, theta2]
+                    if theta1 > theta2, the strip is [theta2, theta1]
             facres(optional): the deflected map is constructed by interpolation of the undeflected map,
                               built at target res. ~ :math:`0.7 * 2^{\rm facres}.` arcmin.
             nband(optional): To avoid dealing with too many large maps in memory, the operations is split in bands.
             verbose(optional): If set, prints a bunch of timing and other info. Defaults to true.
-
+            symmetric_band(optional): If set True, do lensing on the symmetrical bands on north and south hemisphere
+                                      If set False, only do strip lensing on one hemisphere
+                                      
         Returns:
             Deflected healpy maps at resolution nside (real and imaginary parts).
 
@@ -118,7 +163,7 @@ def strip_alm2lenmap_spin(gclm, dlms, nside, spin, theta1, theta2, nband=4, facr
         theta1, theta2 = theta2, theta1
     if not np.iscomplexobj(dlms[0]): assert dlms[0].size == dlms[1].size and dlms[0].size == hp.nside2npix(nside)
     ret = _strip_lens_gclm_sym_timed(spin, dlms[0], gclm[0], nside, theta1, theta2,
-                                clm=gclm[1], dclm=dlms[1], nband=nband, facres=facres, verbose=verbose)
+                                clm=gclm[1], dclm=dlms[1], nband=nband, facres=facres, verbose=verbose, symmetric_band=symmetric_band)
     return ret.real, ret.imag
 
 def _lens_gclm_sym_timed(spin, dlm, glm, nside, nband=8, facres=0, clm=None, dclm=None, verbose=True):
@@ -129,7 +174,7 @@ def _lens_gclm_sym_timed(spin, dlm, glm, nside, nband=8, facres=0, clm=None, dcl
     """
     assert spin >= 0,spin
     times = utils.timer(verbose, suffix=' ' + __name__)
-    target_nt = 3 ** 1 * 2 ** (11 + facres) # on one hemisphere
+    target_nt = 3 ** 1 * 2 ** (12 + facres) # on one hemisphere
 
     #co-latitudes
     th1s = np.arange(nband) * (np.pi * 0.5 / nband)
@@ -195,15 +240,19 @@ def _lens_gclm_sym_timed(spin, dlm, glm, nside, nband=8, facres=0, clm=None, dcl
     return ret
 
 ### Modified by FG
-def _strip_lens_gclm_sym_timed(spin, dlm, glm, nside, theta1, theta2, nband=4, facres=0, clm=None, dclm=None, verbose=True):
+def _strip_lens_gclm_sym_timed(spin, dlm, glm, nside, theta1, theta2, nband=4, facres=0, clm=None, dclm=None, verbose=True, symmetric_band=True):
     """Performs the deflection by splitting the full latitude range into distinct bands which are done one at a time.
 
         See *_lens_gcband_sym* for the single band (north and south hemispheres) lensing.
 
+        symmetric_band: Bool
+            If True, the lensing will be performed in the symmetric bands about the equator
+            If False, only the band in the southern hemisphere covering the SPT3G area will be lensed
+
     """
     assert spin >= 0,spin
     times = utils.timer(verbose, suffix=' ' + __name__)
-    target_nt = 3 ** 1 * 2 ** (11 + facres) # on one hemisphere
+    target_nt = 3 ** 1 * 2 ** (12 + facres) # on one hemisphere
 
     #co-latitudes
     th1s = np.arange(nband) * ((theta2 - theta1) / nband) + theta1
@@ -230,23 +279,26 @@ def _strip_lens_gclm_sym_timed(spin, dlm, glm, nside, theta1, theta2, nband=4, f
         
         ## For MUSE3G, only the southern hemisphere is used. 
         ## So just get rid of the settings for northern hemisphere.
-        #pixn = hp.query_strip(nside, np.pi - th2, np.pi - th1, inclusive=True)
+        ## The northern hemisphere is also included to augment the number of maps of 3g patch generated from one lensing operation
+        pixn = hp.query_strip(nside, np.pi - th2, np.pi - th1, inclusive=True)
         pixs = hp.query_strip(nside, th1, th2, inclusive=True)
-        #thtp, phipn = angles.get_angles(nside, pixn, redtot[pixn], imdtot[pixn], 'north', verbose=verbose)
+        thtpn, phipn = angles.get_angles(nside, pixn, redtot[pixn], imdtot[pixn], 'north', verbose=verbose)
         thtps, phips = angles.get_angles(nside, pixs, redtot[pixs], imdtot[pixs], 'south', verbose=verbose)
 
         # Adding a 10 pixels buffer for new angles to be safely inside interval.
         # th1,th2 is mapped onto pi - th2,pi -th1 so we need to make sure to cover both buffers
-        #mathtp = np.max(thtp); mithtp = np.min(thtp)
-        #print('mathtp=%.5f, mithtp=%.5f'%(mathtp, mithtp))
+        mathtpn = np.max(thtpn); mithtpn = np.min(thtpn)
+        print('mathtp=%.5f, mithtp=%.5f'%(mathtpn, mithtpn))
         mathtps = np.max(thtps); mithtps = np.min(thtps)
         print('mathtps=%.5f, mithtps=%.5f'%(mathtps, mithtps))
-        #buffN = 10 * (mathtp - mithtp) / (nt_perband - 1) / (1. - 2. * 10. / (nt_perband - 1))
+        buffN = 10 * (mathtpn - mithtpn) / (nt_perband - 1) / (1. - 2. * 10. / (nt_perband - 1))
         buffS = 10 * (mathtps - mithtps) / (nt_perband - 1) / (1. - 2. * 10. / (nt_perband - 1))
-        #th1 = min(np.pi - (mathtps + buffS),mithtp - buffN)
-        #th2 = max(np.pi - (mithtps - buffS),mathtp + buffN)
-        th1 = mithtps - buffS
-        th2 = mathtps + buffS
+        if (symmetric_band):
+            th1 = min(np.pi - (mathtps + buffS),mithtpn - buffN)
+            th2 = max(np.pi - (mithtps - buffS),mathtpn + buffN)
+        else:
+            th1 = mithtps - buffS
+            th2 = mathtps + buffS
 
         #==== these are the theta and limits. It is ok to go negative or > 180
         if verbose: print('input t1,t2 %.3f %.3f in degrees'%(th1 /np.pi * 180,th2/np.pi * 180.))
@@ -256,32 +308,40 @@ def _strip_lens_gclm_sym_timed(spin, dlm, glm, nside, theta1, theta2, nband=4, f
         dth_patch = (th2 - th1) / (nt_perband -1)
         if verbose: print("cell (theta,phi) in amin (%.3f,%.3f)" % (dth_patch / np.pi * 60. * 180, dphi_patch / np.pi * 60. * 180))
         times.add('defl. angles calc.')
-#        len_nr, len_ni, len_sr, len_si = _lens_gcband_sym(spin, glm, np.pi-th2, np.pi-th1, nt_perband, nphi, thtp, phipn, thtps, phips,
-#                                                         clm=clm, times=times)
 
-        tgrid = utils.thgrid(np.pi - th2, np.pi - th1).mktgrid(nt_perband)
-        if spin == 0:
-            vtm = shts.glm2vtm_sym(0, utils.thgrid.th2colat(tgrid), glm)
+        if (symmetric_band):
+            len_nr, len_ni, len_sr, len_si = _lens_gcband_sym(spin, glm, th1, th2, nt_perband, nphi, thtpn, phipn, thtps, phips,
+                                                              clm=clm, times=times)
+            if spin == 0:
+                ret[pixn] = len_nr
+                ret[pixs] = len_sr
+            else :
+                ret[pixn] = (len_nr + 1j * len_ni) * angles.rotation(nside, spin, pixn, redtot[pixn], imdtot[pixn])
+                ret[pixs] = (len_sr + 1j * len_si) * angles.rotation(nside, spin, pixs, redtot[pixs], imdtot[pixs])
+                times.add(r'pol. //-transport rot.')
         else:
-            vtm = shts.vlm2vtm_sym(spin, utils.thgrid.th2colat(tgrid), utils.alm2vlm(glm, clm=clm))
-        times.add('vtm')
+            tgrid = utils.thgrid(np.pi - th2, np.pi - th1).mktgrid(nt_perband)
+            if spin == 0:
+                vtm = shts.glm2vtm_sym(0, utils.thgrid.th2colat(tgrid), glm)
+            else:
+                vtm = shts.vlm2vtm_sym(spin, utils.thgrid.th2colat(tgrid), utils.alm2vlm(glm, clm=clm))
+            times.add('vtm')
 
-        # Symmetric band on south hemisphere
-        filtmap = vtm2filtmap(spin, vtm[slice(2 * nt_perband - 1, nt_perband - 1, -1)], nphi, phiflip=np.where((np.pi - tgrid) > np.pi)[0])
-        t_grid = utils.thgrid(th1, th2).togridunits(thtps, nt_perband)
-        p_grid = phips / ((2. * np.pi) / nphi)
-        times.add('vtm2filtmap')
-        lenmapsr = bicubic.deflect(np.require(filtmap.real, np.float64, requirements='F'), t_grid, p_grid)
-        lenmapsi = bicubic.deflect(np.require(filtmap.imag, np.float64, requirements='F'), t_grid, p_grid) if spin > 0 else None
-        times.add('interp')
+            # Symmetric band on south hemisphere
+            filtmap = vtm2filtmap(spin, vtm[slice(2 * nt_perband - 1, nt_perband - 1, -1)], nphi, phiflip=np.where((np.pi - tgrid) > np.pi)[0])
+            t_grid = utils.thgrid(th1, th2).togridunits(thtps, nt_perband)
+            p_grid = phips / ((2. * np.pi) / nphi)
+            times.add('vtm2filtmap')
+            len_sr = bicubic.deflect(np.require(filtmap.real, np.float64, requirements='F'), t_grid, p_grid)
+            len_si = bicubic.deflect(np.require(filtmap.imag, np.float64, requirements='F'), t_grid, p_grid) if spin > 0 else None
+            times.add('interp')
 
-        if spin == 0:
-            #ret[pixn] = len_nr
-            ret[pixs] = lenmapsr
-        else :
-            #ret[pixn] = (len_nr + 1j * len_ni) * angles.rotation(nside, spin, pixn, redtot[pixn], imdtot[pixn])
-            ret[pixs] = (lenmapsr + 1j * lenmapsi) * angles.rotation(nside, spin, pixs, redtot[pixs], imdtot[pixs])
-            times.add(r'pol. //-transport rot.')
+            if spin == 0:
+                ret[pixs] = len_sr
+            else :
+                ret[pixs] = (len_sr + 1j * len_si) * angles.rotation(nside, spin, pixs, redtot[pixs], imdtot[pixs])
+                times.add(r'pol. //-transport rot.')
+        
         interp_pix += nphi * nt_perband * 2
 
     if verbose: print(times)
